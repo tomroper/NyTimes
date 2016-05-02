2cd08c427f0c3bc4737725097289fe61116b2407
tomroper <tomroper2@gmail.com>
2016-05-02 14:43:49 -0700

    update gulpfile to minify the css

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,5 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
-var htmlmin = require('gulp-htmlmin');
 var browserSync = require('browser-sync').create();
 var sass = require('gulp-sass');
 
@@ -15,14 +14,13 @@ gulp.task('browser-sync', function() {
 
     gulp.watch("./src/*.js", ['uglify']);
     gulp.watch('./src/*.scss', ['scss']);
-    gulp.watch('index.html', ['minify']);
     gulp.watch(['./build/**/*.*', 'index.html']).on('change', browserSync.reload);
 });
 
 
 gulp.task('scss', function() {
   return gulp.src('./src/*.scss')
-    .pipe(sass().on('error', sass.logError))
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
     .pipe(gulp.dest('./build'));
 });
 
@@ -31,10 +29,4 @@ gulp.task('uglify', function(){
         gulp.src('./src/*.js') // What files do we want gulp to consume?
             .pipe(uglify()) // Call the uglify function on these files
             .pipe(gulp.dest('./build')) // Where do we put the result?
-});
-
-gulp.task('minify', function() {
-  return gulp.src('*.html')
-    .pipe(htmlmin({collapseWhitespace: true}))
-    .pipe(gulp.dest('./build'))
 });
